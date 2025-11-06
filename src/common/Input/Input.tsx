598e89cdc282e3055eec25742b96b5e0bac2f8fb
Bohdan Henyk <education.bohdan@gmail.com>
2025-11-06 16:18:07 -0700

    Added label and corresponding props to the Input component

--- src/common/Input/Input.tsx
+++ src/common/Input/Input.tsx
@@ -1,15 +1,21 @@
+import { useId } from 'react';
 import './Input.scss';
 
 interface Props {
     className: string;
     placeholderText?: string;
+    labelText?: string;
     inputWidth?: string;
     inputHeight?: string;
 }
 
-const Input: React.FC<Props> = ({ className, placeholderText, inputWidth = '400px', inputHeight = '50px' }) => {
+const Input: React.FC<Props> = ({ className, placeholderText, labelText, inputWidth = '400px', inputHeight = '50px' }) => {
+    const uniqueId = useId();
     return (
-        <input className={className} type="text" placeholder={placeholderText} style={{ width: inputWidth, height: inputHeight }} />
+        <div className='input-container'>
+            <label htmlFor={uniqueId}>{labelText}</label>
+            <input id={uniqueId} className={className} type="text" placeholder={placeholderText} style={{ width: inputWidth, height: inputHeight }} />
+        </div>
     );
 }
 
