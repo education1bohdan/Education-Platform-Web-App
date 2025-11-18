cf753582f305113173d37bbfd12036aa7a44a397
Bohdan Henyk <education.bohdan@gmail.com>
2025-11-18 14:55:33 -0700

    Added form attribute

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -6,11 +6,12 @@ interface props {
     type?: 'button' | 'submit' | 'reset';
     buttonWidth?: string;
     buttonHeight?: string;
+    form?: string;
 }
 
-const LogoutButton: React.FC<props> = ({ type = 'button', buttonText, clickHandler, buttonWidth = '140px', buttonHeight = '50px' }) => {
+const LogoutButton: React.FC<props> = ({ type = 'button', buttonText, clickHandler, buttonWidth = '140px', buttonHeight = '50px', form }) => {
     return (
-        <button type={type} onClick={clickHandler} style={{ width: buttonWidth, height: buttonHeight }}>{buttonText}</button>
+        <button type={type} onClick={clickHandler} form={form} style={{ width: buttonWidth, height: buttonHeight }}>{buttonText}</button>
     )
 }
 
