6d2e5a96e2efcca62d6c63f7d5a4fbdec266da0a
Bohdan Henyk <education.bohdan@gmail.com>
2025-11-18 01:14:18 -0700

    Started working on validation for CreateCourse component, added number validation to the getCourseDuration helper

--- src/components/CreateCourse/CreateCourse.tsx
+++ src/components/CreateCourse/CreateCourse.tsx
@@ -5,69 +5,91 @@ import Button from "../../common/Button/Button";
 import TextArea from '../../common/TextArea/TextArea';
 import AuthorItem from './AuthorItem/AuthorItem';
 import { CREATE_AUTHOR_TEXT, CANCEL, CREATE_COURSE } from '../../constants';
+import getCourseDuration from '../../helpers/getCourseDuration';
+import validateForm from '../../helpers/validateForm';
 import styles from './CreateCourse.module.scss';
 
-let hasError = !false;
+let hasError = false;
 
 interface Props {
 
 }
-
 interface formData {
     title: string;
     description: string;
+    duration: string;
+    authors: string;
+}
+
+export interface ErrorsObject {
+    title?: string;
+    description?: string;
+    duration?: string;
+    authors?: string;
 }
 
 const CreateCourse: React.FC<Props> = ({ }) => {
 
+    const [courseDuration, setCourseDuration] = useState<string[]>(['00:00', 'hours']);
+    const [formErrors, setFormErrors] = useState<ErrorsObject>({});
     const [formData, setFormData] = useState<formData>({
         title: '',
         description: '',
+        duration: '',
+        authors: '',
     })
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>): void => {
         const { name, value } = event.target;
+
         setFormData(prev => ({ ...prev, [name]: value }));
 
-        // if (value.trim()) {
-        //     setFormErrors(prev => {
-        //         const newErrors = { ...prev };
-        //         delete newErrors[name];
-        //         return newErrors;
-        //     });
-        // }
+    }
+
+    const handleChangeDuration = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>): void => {
+        const { value } = event.target;
+        let newValue = value;
+
+        newValue = value.replace(/[^0-9]/g, '');
+
+        setFormData(prev => ({ ...prev, duration: newValue }));
+
+        const durationValue: string[] = getCourseDuration(newValue).split(" ");
+        setCourseDuration(durationValue);
     }
 
     const handleAuthorCreation = (): void => {
 
     }
 
-    const handleCourseCreation = (): void => {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+
 
     }
 
     return (
         <div className={"main-content " + styles.createCourse} >
             <h1>Course Edit/Create Page</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className={styles.mainInfo}>
                     <h2>Main Info</h2>
                     <Input hasError={hasError} errorMessage='Title is required.' inputType='text' name='title' labelText='Title' inputWidth='auto' onChange={handleChange} value={formData.title} />
-                    <TextArea hasError={hasError} errorMessage='Description is required.' name='description' placeholderText='Input Text' labelText='Description' width='auto' height='152px' resize='none' onChange={handleChange} value=''></TextArea>
+                    <TextArea hasError={hasError} errorMessage='Description is required.' name='description' placeholderText='Input Text' labelText='Description' width='auto' height='152px' resize='none' onChange={handleChange} value={formData.description}></TextArea>
                 </div>
                 <div className={styles.duration}>
                     <h2>Duration</h2>
                     <div className={styles['duration-input-container']}>
-                        <Input hasError={hasError} errorMessage='Duration is required.' inputType='text' name='duration' labelText='Duration' inputWidth='400px' onChange={handleChange} value={formData.title} />
+                        <Input hasError={hasError} errorMessage='Duration is required.' inputType='text' name='duration' labelText='Duration' inputWidth='400px' onChange={handleChangeDuration} value={formData.duration} />
                         <div className={styles['duration-p']}>
-                            <p><strong>00:00</strong> hours</p>
+                            <p><strong>{courseDuration[0]}</strong> {courseDuration[1]}</p>
                         </div>
                     </div>
                 </div>
                 <div className={styles.authors}>
                     <h2>Authors</h2>
                     <div className={styles['authors-input-container']}>
-                        <Input hasError={hasError} inputType='text' name='authors' labelText='Author Name' inputWidth='400px' onChange={handleChange} value={formData.title} />
+                        <Input hasError={hasError} inputType='text' name='authors' labelText='Author Name' inputWidth='400px' onChange={handleChange} value={formData.authors} />
                         <Button buttonText={CREATE_AUTHOR_TEXT} clickHandler={handleAuthorCreation} buttonWidth="185px" />
                     </div>
                     <div className={styles['authors-list-container']}>
@@ -87,7 +109,7 @@ const CreateCourse: React.FC<Props> = ({ }) => {
             </form>
             <div className={styles['button-container']}>
                 <Link to='/'><Button buttonText={CANCEL} buttonWidth="185px" /></Link>
-                <Button buttonText={CREATE_COURSE} clickHandler={handleCourseCreation} buttonWidth="185px" />
+                <Button type='submit' buttonText={CREATE_COURSE} buttonWidth="185px" />
             </div>
         </div>
     )
--- src/helpers/getCourseDuration.ts
+++ src/helpers/getCourseDuration.ts
@@ -1,11 +1,24 @@
-function getCourseDuration(data: number): string {
-    let m: number = data % 60;
-    let h: number = (data - m) / 60;
+function getCourseDuration(value: number | string): string {
+
+    let durationValue: number = Number(value);
+
+    if (isNaN(durationValue)) {
+        durationValue = 0;
+    }
+
+    let m: number = durationValue % 60;
+    let h: number = (durationValue - m) / 60;
 
     let hh: string | number = h < 10 ? `0${h}` : h;
     let mm: string | number = m < 10 ? `0${m}` : m;
 
-    return h === 1 ? `${hh}:${mm} hour` : `${hh}:${mm} hours`
+    if (h === 0 && m === 1) {
+        return `${hh}:${mm} minute`;
+    } else if (h === 0) {
+        return `${hh}:${mm} minutes`;
+    }
+
+    return h === 1 ? `${hh}:${mm} hour` : `${hh}:${mm} hours`;
 }
 
 export default getCourseDuration;
