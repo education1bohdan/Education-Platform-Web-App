ad523effcf9cf1055fafcbc3c6915b09df6e827c
Bohdan Henyk <education.bohdan@gmail.com>
2025-11-18 23:04:27 -0700

    Divided form validation logic into two helpers

---
+++ src/helpers/validateCreateCourse.ts
@@ -0,0 +1,23 @@
+import { ErrorsObject } from '../components/Authentification/Registration/Registration';
+function validateCreateCourse<T extends Record<string, string>>(object: T, minLength: number): ErrorsObject | {} {
+    const errorsObject: Partial<Record<keyof T, string>> = {};
+
+    for (let key in object) {
+
+        const inputName = key.charAt(0).toUpperCase() + key.slice(1);
+
+        if (!object[key].trim()) {
+            errorsObject[key] = `${inputName} is required`;
+        } else if ((key !== 'duration') && (object[key].length < minLength)) {
+            if (key === 'authors') {
+                errorsObject[key] = `The ${key}' names should be at least ${minLength} characters long`;
+            } else {
+                errorsObject[key] = `The ${key} should be at least ${minLength} characters long`;
+            }
+        } else if ((key === 'duration') && (object[key] === '0')) {
+            errorsObject[key] = `The ${key} should be more than 0`;
+        }
+    }
+    return errorsObject;
+}
+export default validateCreateCourse;
