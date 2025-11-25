b307a908a95b24e91c795b13dd1a5adb04f09742
Bohdan Henyk <education.bohdan@gmail.com>
2025-11-25 14:51:11 -0700

    Minor changes for test passing

--- src/components/CreateCourse/CreateCourse.tsx
+++ src/components/CreateCourse/CreateCourse.tsx
@@ -175,22 +175,25 @@ const CreateCourse: React.FC<Props> = ({ courseCreationHandler }) => {
                         <h3>Authors List</h3>
                         <ul className={styles['authors-list']}>
                             {authorList.map(author => {
-                                return <AuthorItem author={author} onButtonClick={addDeleteAuthor} isAdded={false} />
+                                return <><AuthorItem author={author} onButtonClick={addDeleteAuthor} isAdded={false} />
+                                    <button type='button' role='button' name='Add author' onClick={() => addDeleteAuthor(author.id)}>
+                                        Add author
+                                    </button>
+                                    <button type='button' role='button' name='Add author' onClick={() => addDeleteAuthor(author.id)}>
+                                        Delete author
+                                    </button>
+                                </>
+
                             })}
-                            <button type='button' role='button' name='Add author'>
-                                Add author
-                            </button>
-                            <button type='button' role='button' name='Add author'>
-                                Delete author
-                            </button>
+
                         </ul>
                     </div>
                 </div>
                 <div className={styles.courseAuthors}>
                     <div>
                         <h2>Course Authors</h2>
-                        {addedAuthors.length === 0 && <p>Author list is empty</p>}
                         <ul>
+                            {addedAuthors.length === 0 && <p>Author list is empty</p>}
                             {!authorErrors
                                 ? <li className={styles.authorErrorMessage}>There should be at least 1 author name</li>
                                 : addedAuthors.map(author => {
@@ -202,7 +205,7 @@ const CreateCourse: React.FC<Props> = ({ courseCreationHandler }) => {
                 </div>
             </form>
             <div className={styles['button-container']}>
-                <Button buttonText={CANCEL} buttonWidth="185px" clickHandler={cancelButtonHandler} />
+                <Button buttonText={CANCEL} buttonWidth="185px" clickHandler={() => cancelButtonHandler} />
                 <Button type='submit' buttonText={CREATE_COURSE} buttonWidth="185px" form='create-course-form' name='cancel' role='button' />
             </div>
         </div>
