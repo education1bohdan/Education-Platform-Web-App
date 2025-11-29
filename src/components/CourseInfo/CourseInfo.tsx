d2b225e62d4e66638fcb8693701719d9b9622dd1
Bohdan Henyk <education.bohdan@gmail.com>
2025-11-28 20:05:32 -0700

    feat(router): Implement routing to courseInfo components

--- src/components/CourseInfo/CourseInfo.tsx
+++ src/components/CourseInfo/CourseInfo.tsx
@@ -1,46 +1,65 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Button from "../../common/Button/Button";
-import { BACK_TEXT } from "../../constants";
-import "./CourseInfo.scss";
+import { Course, Authors } from "../Courses/Courses";
+import formatCreationDate from "../../helpers/formatCreationDate";
 import getCourseDuration from "../../helpers/getCourseDuration";
-import { Props } from "../Courses/components/CourseCard/CourseCard";
+import getAuthorsNames from "../../helpers/getAuthorsNames";
+import { BACK_TEXT } from "../../constants";
+import styles from "./CourseInfo.module.scss";
 
-const CourseInfo: React.FC<Props> = ({ id, title, description, creationDate, duration, authors }) => {
-    const durationText: string = getCourseDuration(duration);
-    const [firstPart, secondPart] = durationText.split(' ');
 
-    return (
-        <div className='main-content'>
-            <div className="course-info">
-                <h1>{title}</h1>
-                <div className="course-info-container">
-                    <h2>Description:</h2>
-                    <div className="course-info-content">
-                        <div className="left-side">
-                            <p>{description}</p>
-                        </div>
-                        <div className="right-side">
-                            <div className="division-line"></div>
-                            <ul className="courses-info-labels">
-                                <li><b>ID:</b></li>
-                                <li><b>Duration:</b></li>
-                                <li><b>Created:</b></li>
-                                <li><b>Authors:</b></li>
-                            </ul>
-                            <ul className="courses-info-details">
-                                <li>{id}</li>
-                                <li><b>{firstPart}</b> {secondPart}</li>
-                                <li>{creationDate}</li>
-                                <li>{authors}</li>
-                            </ul>
+interface Props {
+    coursesList: Course[];
+    authorsList: Authors[];
+}
+
+const CourseInfo: React.FC<Props> = ({ coursesList, authorsList }) => {
+    const { courseId } = useParams();
+    const course = coursesList.find(c => c.id === courseId);
+    if (!course) {
+        return (
+            <div className={"main-content " + styles.notFound}>
+                <h1>Course Not Found</h1>
+            </div>
+        )
+    } else {
+        const { id, title, description, creationDate, duration, authors } = course;
+        const durationText: string = getCourseDuration(duration);
+        const [firstPart, secondPart] = durationText.split(' ');
+
+        return (
+            <div className='main-content'>
+                <div className={styles.courseInfo}>
+                    <h1>{title}</h1>
+                    <div className={styles.courseInfoContainer}>
+                        <h2>Description:</h2>
+                        <div className={styles.courseInfoContent}>
+                            <div className={styles.leftSide}>
+                                <p>{description}</p>
+                            </div>
+                            <div className={styles.rightSide}>
+                                <div className={styles.divisionLine}></div>
+                                <ul className={styles.coursesInfoLabels}>
+                                    <li><strong>ID:</strong></li>
+                                    <li><strong>Duration:</strong></li>
+                                    <li><strong>Created:</strong></li>
+                                    <li><strong>Authors:</strong></li>
+                                </ul>
+                                <ul className={styles.coursesInfoDetails}>
+                                    <li>{id}</li>
+                                    <li><strong>{firstPart}</strong> {secondPart}</li>
+                                    <li>{formatCreationDate(creationDate)}</li>
+                                    <li>{getAuthorsNames(authors, authorsList)}</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
+                    <Link to="/"><Button buttonText={BACK_TEXT} buttonWidth="185px" /></Link>
                 </div>
-                <Link to="/"><Button buttonText={BACK_TEXT} buttonWidth="185px" /></Link>
             </div>
-        </div>
 
-    );
+        );
+    }
 }
 
 export default CourseInfo;
